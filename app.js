6355befdef67d7e9f05bbc2f976fc57199d0d823
chao69 <star010631@gmail.com>
2021-08-24 21:40:06 +0800

    Add flash message when sign up fial

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 
 const db = require('./models')
 const app = express()
@@ -9,6 +11,14 @@ const port = 3000
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: flash, saveUninitialized: false }))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_message = req.flash('success_message')
+  res.locals.error_message = req.flash('error_message')
+  next()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- controllers/userController.js
+++ controllers/userController.js
@@ -8,16 +8,27 @@ const userController = {
   },
 
   signUp: (req, res) => {
-    console.log(req.body)
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-    })
-      .then(user => {
-        return res.redirect('/signin')
-      })
+    if (req.body.password !== req.body.passwordCheck) {
+      req.flash('error_message', '密碼與確認密碼不相符！')
+      return res.redirect('/signup')
+    } else {
+      User.findOne({ where: { email: req.body.email } })
+        .then(user => {
+          if (user) {
+            req.flash('error_message', '此信箱已註冊')
+            return res.redirect('/signup')
+          } else {
+            User.create({
+              name: req.body.name,
+              email: req.body.email,
+              password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+            })
+          }
+        })
+        .then(user => {
+          return res.redirect('/signin')
+        })
+    }
   }
 }
-
 module.exports = userController
