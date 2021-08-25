2f0c4b8a83f1592a25dd417d5877fbc92e1b2307
chao69 <star010631@gmail.com>
2021-08-25 22:23:41 +0800

    Add post Restaurant function

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,6 +6,28 @@ const adminController = {
     return Restaurant.findAll({ raw: true }).then(restaurants => {
       return res.render('admin/restaurants', { restaurants })
     })
+  },
+  createRestaurant: (req, res) => {
+    return res.render('admin/create')
+  },
+  postRestaurant: (req, res) => {
+    const { name, tel, address, opening_hours, description } = req.body
+
+    if (!req.body.name) {
+      req.flash('error_message', "name didn't exist")
+      return res.redirect('back')
+    }
+    return Restaurant.create({
+      name,
+      tel,
+      address,
+      opening_hours,
+      description
+    })
+      .then(restaurant => {
+        req.flash('success_message', 'restaurant was successfully created')
+        res.redirect('/admin/restaurants')
+      })
   }
 }
 
