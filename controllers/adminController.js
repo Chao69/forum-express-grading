25071055211e97a8002073e8a562a7ff6b713431
chao69 <star010631@gmail.com>
2021-08-26 01:04:33 +0800

    Debuging

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+
 const db = require('../models')
 const Restaurant = db.Restaurant
 
@@ -7,60 +9,104 @@ const adminController = {
       return res.render('admin/restaurants', { restaurants })
     })
   },
+
   createRestaurant: (req, res) => {
     return res.render('admin/create')
   },
+
   postRestaurant: (req, res) => {
     const { name, tel, address, opening_hours, description } = req.body
-
     if (!req.body.name) {
-      req.flash('error_message', "name didn't exist")
+      req.flash('error_messages', "name didn't exist")
       return res.redirect('back')
     }
-    return Restaurant.create({
-      name,
-      tel,
-      address,
-      opening_hours,
-      description
-    })
-      .then(restaurant => {
-        req.flash('success_message', 'restaurant was successfully created')
-        res.redirect('/admin/restaurants')
+
+    const { file } = req 
+    if (file) {
+      fs.readFile(file.path, (err, data) => {
+        if (err) console.log('Error: ', err)
+        fs.writeFile(`upload/${file.originalname}`, data, () => {
+          return Restaurant.create({
+            name,
+            tel,
+            address,
+            opening_hours,
+            description,
+            image: file ? `/upload/${file.originalname}` : null
+          }).then((restaurant) => {
+            req.flash('success_messages', 'restaurant was successfully created')
+            return res.redirect('/admin/restaurants')
+          })
+        })
       })
+    } else {
+      return Restaurant.create({
+        name,
+        tel,
+        address,
+        opening_hours,
+        description,
+        image: null
+      }).then((restaurant) => {
+        req.flash('success_messages', 'restaurant was successfully created')
+        return res.redirect('/admin/restaurants')
+      })
+    }
   },
-  getRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, { raw: true }).then(restaurant => {
-      return res.render('admin/restaurant', { restaurant })
-    })
-  },
+
   editRestaurant: (req, res) => {
     return Restaurant.findByPk(req.params.id, { raw: true }).then(restaurant => {
       return res.render('admin/create', { restaurant })
     })
   },
+
   putRestaurant: (req, res) => {
     const { name, tel, address, opening_hours, description } = req.body
-
+    
     if (!req.body.name) {
-      req.flash('error_message', "name didn't exist")
+      req.flash('error_messages', "name didn't exist")
       return res.redirect('back')
     }
-    return Restaurant.findByPk(req.params.id)
-      .then((restaurant) => {
-        restaurant.update({
-          name,
-          tel,
-          address,
-          opening_hours,
-          description
+
+    const { file } = req
+    if (file) {
+      fs.readFile(file.path, (err, data) => {
+        if (err) console.log('Error: ', err)
+        fs.writeFile(`upload/${file.originalname}`, data, () => {
+          return Restaurant.findByPk(req.params.id)
+            .then((restaurant) => {
+              restaurant.update({
+                name,
+                tel,
+                address,
+                opening_hours,
+                description,
+                image: file ? `/upload/${file.originalname}` : restaurant.image
+              }).then((restaurant) => {
+                req.flash('success_messages', 'restaurant was successfully to update')
+                res.redirect('/admin/restaurants')
+              })
+            })
         })
-          .then(restaurant => {
-            req.flash('success_message', 'restaurant was successfully update')
+      })
+    } else {
+      return Restaurant.findByPk(req.params.id)
+        .then((restaurant) => {
+          restaurant.update({
+            name,
+            tel,
+            address,
+            opening_hours,
+            description,
+            image: restaurant.image
+          }).then((restaurant) => {
+            req.flash('success_messages', 'restaurant was successfully to update')
             res.redirect('/admin/restaurants')
           })
-      })
+        })
+    }
   },
+
   deleteRestaurant: (req, res) => {
     return Restaurant.findByPk(req.params.id)
       .then(restaurant => {
---
+++ migrations/20210825160529-add-image-to-restaurants.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const { sequelize } = require("../models");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Restaurants', 'image', {
+      type: Sequelize.STRING
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Restaurants', 'image');
+  }
+};
