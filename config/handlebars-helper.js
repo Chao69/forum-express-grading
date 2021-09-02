2f36ee3aec18e812828370fbeebc095427332e30
chao69 <star010631@gmail.com>
2021-09-02 23:44:56 +0800

    Add all comment to restaurant page and using moment format time

--- config/handlebars-helper.js
+++ config/handlebars-helper.js
@@ -1,8 +1,14 @@
+const moment = require('moment')
+
 module.exports = {
   ifCond: function (a, b, option) {
     if(a === b) {
       return option.fn(this)
     }
     return option.inverse(this)
+  },
+
+  moment: function (a) {
+    return moment(a).fromNow()
   }
 }
--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,8 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const Comment = db.Comment
+const User = db.User
 
 const pageLimit = 10
 
@@ -16,11 +18,11 @@ const restController = {
     if (req.query.page) {
       offset = (req.query.page - 1) * pageLimit
     }
-    Restaurant.findAndCountAll({ 
-      include: Category, 
+    Restaurant.findAndCountAll({
+      include: Category,
       where: whereQuery,
       offset: offset,
-      limit: pageLimit 
+      limit: pageLimit
     })
       .then(result => {
         const page = Number(req.query.page) || 1
@@ -37,7 +39,7 @@ const restController = {
           raw: true,
           nest: true
         }).then(categories => {
-          return res.render('restaurants', { 
+          return res.render('restaurants', {
             restaurants: data,
             categories,
             categoryId,
@@ -51,10 +53,15 @@ const restController = {
   },
 
   getRestaurant: (req, res) => {
-    Restaurant.findByPk(req.params.id, { include: Category })
-      .then(restaurant => {
-        return res.render('restaurant', { 
-          restaurant: restaurant.toJSON() 
+    Restaurant.findByPk(req.params.id, {
+      include: [
+        Category,
+        { model: Comment, include: [User] }
+      ]
+    }).then(restaurant => {
+      console.log(restaurant.Comments[0].dataValues)
+        return res.render('restaurant', {
+          restaurant: restaurant.toJSON()
         })
       })
   }
