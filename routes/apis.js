35ba059eae4e5cd8458971bbcaef06322dbeb008
chao69 <star010631@gmail.com>
2021-09-09 15:15:16 +0800

    refactor adminController.putRestaurant

--- routes/apis.js
+++ routes/apis.js
@@ -6,10 +6,12 @@ const router = express.Router()
 
 const adminController = require('../controllers/api/adminController')
 const categoryController = require('../controllers/api/categoryController')
+const adminService = require('../services/adminService')
 
 router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
 router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
+router.put('/admin/restaurants/:id', upload.single('image'), adminService.putRestaurant)
 router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
 
 
--- services/adminService.js
+++ services/adminService.js
@@ -72,6 +72,50 @@ const adminService = {
           callback({ status: 'success', message: 'restaurant was successfully created' })
         })
     }
+  },
+
+  putRestaurant: (req, res, callback) => {
+    const { name, tel, address, opening_hours, description } = req.body
+
+    if (!req.body.name) {
+      return callback({ status: 'error', message: "name didn't exist" })
+    }
+
+    const { file } = req
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID);
+      imgur.upload(file.path, (err, img) => {
+        return Restaurant.findByPk(req.params.id)
+          .then((restaurant) => {
+            restaurant.update({
+              name,
+              tel,
+              address,
+              opening_hours,
+              description,
+              image: file ? img.data.link : restaurant.image,
+              CategoryId: req.body.categoryId
+            }).then((restaurant) => {
+              callback({ status: 'success', message: 'restaurant was successfully to update' })
+            })
+          })
+      })
+    } else {
+      return Restaurant.findByPk(req.params.id)
+        .then((restaurant) => {
+          restaurant.update({
+            name,
+            tel,
+            address,
+            opening_hours,
+            description,
+            image: restaurant.image,
+            CategoryId: req.body.categoryId
+          }).then((restaurant) => {
+            callback({ status: 'success', message: 'restaurant was successfully to update' })
+          })
+        })
+    }
   }
 }
 
